d1745fbc85b8d2e666aad707d58abdf09c5f8b42
ymatin <spacemarineharon@mail.ru>
2019-02-28 23:14:14 +0300

    настроил поведение шкалы рейтинга

--- src/FeedBackForm/FeedbackFormElement/FeedbackFormElement.js
+++ src/FeedBackForm/FeedbackFormElement/FeedbackFormElement.js
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import './FeedbackFormElement.css';
 import StarRatingComponent from 'react-star-rating-component';
 import * as AnswerType from '../../common/answerTypes'
 
-const FeedbackFormElement = props => {
-    let renderedComponent = null;
+class FeedbackFormElement extends Component {
+    state = {
+        value: 0,
+        selected: 0
+    };
 
-    switch (props.questionObject.answerType) {
-        case AnswerType.STAR: {
-            renderedComponent = <StarRatingComponent name={props.questionObject.questionName}/>
+    render() {
+        let renderedComponent = null;
+        switch (this.props.questionObject.answerType) {
+            case AnswerType.STAR: {
+                renderedComponent = <StarRatingComponent name={this.props.questionObject.questionName}/>;
+                break;
+            }
+            default:
+                renderedComponent = null;
+                break;
         }
-    }
 
-    return (
-        <div>
-            <StarRatingComponent
-                name="rate1"
-                starCount={10}
-                value={1}
-            />
-        </div>
-    );
-};
+        return (
+            <div className='FeedbackFormElement'>
+                <StarRatingComponent
+                    name="rate1"
+                    starCount={5}
+                    value={this.state.value}
+                    onStarClick={(nextValue, prevValue, name) => {
+                        this.setState({value: nextValue, selected: nextValue})
+                    }}
+                    onStarHover={(nextValue, prevValue, name) => {
+                        this.setState({value: nextValue})
+                    }}
+                    onStarHoverOut={(nextValue, prevValue, name) => {
+                        const selected = this.state.selected;
+                        this.setState({value: selected})
+                    }}
+                />
+            </div>
+        );
+    }
+}
 
 FeedbackFormElement.propTypes = {
     questionObject: PropTypes.object.isRequired
